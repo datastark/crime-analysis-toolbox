469eb2b054221dbee89fcbfe6f3f78d5f989afae
Allison Muise <amuise@esri.com>
2016-02-24 15:31:11 -0800

    refactored script & added validation to tbx

--- calculate_prediction_zones.py
+++ calculate_prediction_zones.py
@@ -1,17 +1,16 @@
 # -----------------------------------------------------------------------------
-# Name:         calculate_prediction_zones.py
+# Name:         calculate_prediction_zones.py BETA
 #
 # Purpose:      Calculate a probability surface showing the liklihood of the
 #               occurance of a repeat or near repeat incident
 #
-# Author:       Allison Muise
+# Author:       Esri., Inc.
 #
 # Created:     04/02/2016
 # -----------------------------------------------------------------------------
 
-# D:\CrimeAnalysis\TestData.gdb\TwoWeeks Date_ 1000 14 CUMULATIVE 5 D:\CrimeAnalysis\TestData.gdb\testraster D:\CrimeAnalysis\TestData.gdb\testpolys2 True False ARCGIS_ONLINE amuise_lg pigsfly http://arcgis4localgov2.maps.arcgis.com http://services.arcgis.com/b6gLrKHqgkQb393u/arcgis/rest/services/TestPolys/FeatureServer/0
-
 import arcpy
+
 from datetime import datetime as dt
 from datetime import timedelta as td
 from os import path
@@ -25,8 +24,13 @@ from arcrest.agol import FeatureLayer
 # Enable overwriting datasets
 arcpy.env.overwriteOutput = True
 
+# Status fields for output polygons (created if non-existant)
 cur_status_field = 'MOSTRECENT'
 cur_date_field = 'CREATEDATE'
+
+# TODO: Get current date & time
+##        today = dt.today()
+today = dt.strptime("12/15/2009", "%m/%d/%Y")
 
 def expand_extents(data, stretch):
     """Expand the extents of a dataset by a set distance in all directions by
@@ -52,27 +56,30 @@ def expand_extents(data, stretch):
             if row[0] == xmin or row[1] == ymax:
                 rows.deleteRow()
 
+# End of expand_extents function
+
 
 def connect_to_layer(username, password, server_url, service_url):
+    """Connect to ArcGIS Online or Portal for ArcGIS layer"""
     proxy_port = None
     proxy_url = None
 
-    securityinfo = {}
-    securityinfo['security_type'] = 'Portal'#LDAP, NTLM, OAuth, Portal, PKI
-    securityinfo['username'] = username
-    securityinfo['password'] = password
-    securityinfo['org_url'] = server_url
-    securityinfo['proxy_url'] = proxy_url
-    securityinfo['proxy_port'] = proxy_port
-    securityinfo['referer_url'] = None
-    securityinfo['token_url'] = None
-    securityinfo['certificatefile'] = None
-    securityinfo['keyfile'] = None
-    securityinfo['client_id'] = None
-    securityinfo['secret_id'] = None
+    si = {}
+    si['security_type'] = 'Portal'  # LDAP, NTLM, OAuth, Portal, PKI
+    si['username'] = username
+    si['password'] = password
+    si['org_url'] = server_url
+    si['proxy_url'] = proxy_url
+    si['proxy_port'] = proxy_port
+    si['referer_url'] = None
+    si['token_url'] = None
+    si['certificatefile'] = None
+    si['keyfile'] = None
+    si['client_id'] = None
+    si['secret_id'] = None
 
-    shh = securityhandlerhelper.securityhandlerhelper(securityinfo=securityinfo)
-    if shh.valid == False:
+    shh = securityhandlerhelper.securityhandlerhelper(securityinfo=si)
+    if not shh.valid:
         raise Exception(shh.message)
 
     fl = FeatureLayer(
@@ -84,16 +91,138 @@ def connect_to_layer(username, password, server_url, service_url):
 
     return fl
 
+# End of connect_to_layer function
+
+
+def calculate_risk_surface(lyr, age, dist):
+    """Create raster of risk extent and intensity based on incident age and
+       spatial reach of influence"""
+
+    # Build float distance raster for incident
+    dist_raster = arcpy.sa.EucDistance(lyr,
+                                       dist)
+
+    # Apply distance & temporal decay
+    inc_raster = (float(dist) - (dist_raster + 1.0)) * (float(2) / float(age + 1))
+
+    # Set Null values to 0 to allow for raster math
+    null_locations = arcpy.sa.IsNull(inc_raster)
+    inc_raster = arcpy.sa.Con(null_locations, 0,
+                              inc_raster, where_clause="Value = 1")
+    return inc_raster
+
+# End of calculate_risk_surface function
+
+
+def calculate_max_risk(cumulative_raster, new_raster):
+    """Creates a raster using the maximum values of two rasters"""
+    # Determine cells where sum raster has => value
+    sum_vals = arcpy.sa.GreaterThanEqual(cumulative_raster, new_raster)
+    sum_vals = arcpy.sa.Times(sum_vals, cumulative_raster)
+
+    # Determine cells where inc raster has greater value
+    inc_vals = arcpy.sa.GreaterThan(new_raster, cumulative_raster)
+    inc_vals = arcpy.sa.Times(inc_vals, new_raster)
+
+    # Sum greatest value rasters
+    cumulative_raster += inc_vals
+
+    return cumulative_raster
+
+# End of calculate_max_risk function
+
+
+def add_status_fields_to_lyr(lyr):
+    """Adds a text and/or date field to a fc or lyr for tracking status"""
+    fields = [f.name for f in arcpy.ListFields(lyr)]
+
+    if cur_status_field not in fields:
+        arcpy.AddField_management(lyr, cur_status_field, 'TEXT', field_length=5)
+
+    if cur_date_field not in fields:
+        arcpy.AddField_management(lyr, cur_date_field, 'DATE')
+
+# End of add_status_fields_to_lyr function
+
+
+def add_status_field_to_service(fl):
+    """Adds a text and/or date field to a hosted service for tracking status"""
+    layer_fields = [f['name'] for f in fl.fields]
+    fieldToAdd = {"fields": []}
+
+    if cur_status_field not in layer_fields:
+        fieldToAdd["fields"].append({
+                "name": cur_status_field,
+                "type": "esriFieldTypeString",
+                "alias": cur_status_field,
+                "sqlType": "sqlTypeOther",
+                "length": 5,
+                "nullable": True,
+                "editable": True,
+                "domain": None,
+                "defaultValue": None})
+
+    if cur_date_field not in layer_fields:
+        fieldToAdd["fields"].append({
+                "name": cur_date_field,
+                "type": "esriFieldTypeDate",
+                "alias": cur_date_field,
+                "nullable": True,
+                "editable": True,
+                "domain": None,
+                "defaultValue": None})
+
+    fl.administration.addToDefinition(fieldToAdd)
+
+# End of add_status_field_to_service function
+
+
+def convert_raster_to_zones(raster, bins, status_field, date_field):
+    """Convert non-0 raster cell values to polygons using a
+       set number of bins"""
+    sliced = arcpy.sa.Slice(raster, int(bins))
+    polys = arcpy.RasterToPolygon_conversion(sliced,
+                                             path.join("in_memory",
+                                                       "temp_polys"),
+                                             "NO_SIMPLIFY")
+    add_status_fields_to_lyr(polys)
+
+    with arcpy.da.UpdateCursor(polys, [status_field, date_field]) as rows:
+        for row in rows:
+            row[0] = 'True'
+            row[1] = today
+            rows.updateRow(row)
+
+    return polys
+
+# End of convert_raster_to_zones function
+
+
+def create_zone_fc(template, sr, out_path):
+    """Create polygon feature class for prediction zone features"""
+    poly_paths = out_path.split(os.sep)[:-1]
+    poly_path = os.sep.join(poly_paths)
+    poly_name = out_path.split(os.sep)[-1]
+
+    arcpy.CreateFeatureclass_management(poly_path,
+                                        poly_name,
+                                        template=template)
+    arcpy.DefineProjection_management(out_path, sr)
+
+    return out_path
+
+# End of create_zone_fc function
+
 
 def main(in_features, date_field, spatial_band_size, temporal_band_size,
          probability_type, slice_num, out_raster, out_polygon,
-         pub_raster, pub_polys, pub_type,
-         username, password, server_url, poly_url, *args):
+         pub_polys='', pub_type='', username='', password='',
+         server_url='', poly_url='', *args):
 
     """ Generates a raster and series of polygons based on that raster to
         illustrate the probability of incidents occuring at the current moment
-        in time based on defined algorithms for the decay of spatial and temporal
-        influence of previous incidents.
+        in time based on defined algorithms for the decay of spatial and
+        temporal influence of previous incidents.
 
         in_features: Point feature class showing the location of incidents that
                      have occured recently, and from which predictions will be
@@ -133,15 +262,20 @@ def main(in_features, date_field, spatial_band_size, temporal_band_size,
                         out_raster values into slice_num categories.
                         Polygon boundaries represent the bounds of the
                         prediction zones as defined by the raster slices.
+
         pub_polys: booleen option for publishing the polygon features. Service
                    must exist previously. Service will be truncated and the
                    cumulative results from in_features will be appended
-        pub_raster: booleen option for publishing the raster.
+
         pub_type: Choice of publication environments- NONE, ARCGIS_ONLINE,
                   ARCGIS_PORTAL, ARCGIS_SERVER
-        username: administrative usernmae for the services
+
+        username: administrative username for the services
+
         password: corresponding to the username
+
         server_url: organization url
+
         poly_url: URL to the rest endpoint of the polygon service layer
     """
 
@@ -157,8 +291,6 @@ def main(in_features, date_field, spatial_band_size, temporal_band_size,
         # Convert booleen values
         if not pub_polys == 'True':
             pub_polys = False
-        if not pub_raster == 'True':
-            pub_raster = False
 
         # Work in an in-memory copy of the dataset to avoid editing the original
         incident_fc = arcpy.FeatureClassToFeatureClass_conversion(in_features,
@@ -176,110 +308,68 @@ def main(in_features, date_field, spatial_band_size, temporal_band_size,
         # SelectLayerByAttributes tool requires feature layer
         incident_lyr = arcpy.MakeFeatureLayer_management(incident_fc)
 
-        # Create summary raster with max extents
+        # Create in-memory summary raster with max extents
         d = arcpy.Describe(incident_fc)
+        sr = d.spatialReference
         arcpy.env.extent = d.extent
 
-        sum_raster = arcpy.sa.CreateConstantRaster(0,
-                                                   data_type='INTEGER',
+        sum_raster = arcpy.sa.CreateConstantRaster(0, data_type='INTEGER',
                                                    extent=d.extent)
 
-        # TODO: Get current date & time
-##        today = dt.today() # Data is old
-        today = dt.strptime("11/15/2009", "%m/%d/%Y")
+        # Calculate minimum bounds of accepted time frame
         date_min = today - td(days=int(temporal_band_size))
 
-        where_clause = """{0} <= date'{1}' AND {0} >= date'{2}'""".format(date_field, today, date_min)
+        # Create risk rasters for each incident within temporal reach of today
+        sql = """{0} <= date'{1}' AND {0} >= date'{2}'""".format(date_field,
+                                                                 today,
+                                                                 date_min)
 
-        # Create spatial temporal rasters for each incident
-        with arcpy.da.SearchCursor(incident_fc, ['OID@', date_field], where_clause=where_clause) as incidents:
+        with arcpy.da.SearchCursor(incident_fc,
+                                   ['OID@', date_field],
+                                   where_clause=sql) as incidents:
+
             for incident in incidents:
 
                 # Calculate age of incident
                 date_diff = today - incident[1]
 
                 # Build float distance raster for incident
-                where_clause = """{} = {}""".format(oidname, incident[0])
+                sql = """{} = {}""".format(oidname, incident[0])
                 arcpy.SelectLayerByAttribute_management(incident_lyr,
-                                                        where_clause=where_clause)
-                dist_raster = arcpy.sa.EucDistance(incident_lyr,
-                                                   spatial_band_size)
+                                                        where_clause=sql)
 
-                # Apply distance & temporal decay
-                inc_raster = (float(spatial_band_size) - (dist_raster + 1.0)) * (float(2)/float(date_diff.days +1))
-
-                # Set Null values to 0 to allow for raster math
-                null_locations = arcpy.sa.IsNull(inc_raster)
-                inc_raster = arcpy.sa.Con(null_locations, 0,
-                                          inc_raster, where_clause="Value = 1")
+                inc_raster = calculate_risk_surface(incident_lyr,
+                                                    date_diff.days,
+                                                    spatial_band_size)
 
                 # Process cumulative risk
                 if probability_type == 'CUMULATIVE':
-                    sum_raster = arcpy.sa.Plus(sum_raster, inc_raster)
+                    sum_raster += inc_raster
 
                 # Process maximum risk
                 else:
-                    # Determine cells where sum raster has => value
-                    sum_vals = arcpy.sa.GreaterThanEqual(sum_raster, inc_raster)
-                    sum_vals = arcpy.sa.Times(sum_vals, sum_raster)
+                    sum_raster = calculate_max_risk(sum_raster, inc_raster)
 
-                    # Determine cells where inc raster has greater value
-                    inc_vals = arcpy.sa.GreaterThan(inc_raster, sum_raster)
-                    inc_vals = arcpy.sa.Times(inc_vals, inc_raster)
-
-                    # Sum greatest value rasters
-                    sum_raster = arcpy.sa.Plus(sum_vals, inc_vals)
-
-        # Save final probability raster
+        # Save final probability raster where values are > 0
         sum_raster = arcpy.sa.SetNull(sum_raster, sum_raster, "Value <= 0")
-        out_raster = '_'.join([out_raster, now])
-        sum_raster.save(out_raster)
+        sum_raster.save('_'.join([out_raster, now]))
 
         # Slice raster values into categories and convert to temp polys
-        slice_raster = arcpy.sa.Slice(sum_raster, int(slice_num))
-        temp_polys = arcpy.RasterToPolygon_conversion(slice_raster,
-                                                      path.join("in_memory",
-                                                                "temp_polys"),
-                                                      "NO_SIMPLIFY")
-        arcpy.AddField_management(temp_polys,
-                                  cur_status_field,
-                                  'TEXT',
-                                  field_length=5)
-
-        arcpy.AddField_management(temp_polys,
-                                  cur_date_field,
-                                  'DATE')
-
-        with arcpy.da.UpdateCursor(temp_polys, [cur_status_field, cur_date_field]) as rows:
-            for row in rows:
-                row[0] = 'True'
-                row[1] = today
-                rows.updateRow(row)
+        temp_polys = convert_raster_to_zones(sum_raster, slice_num,
+                                             cur_status_field, cur_date_field)
 
         # Creat polygon fc if it doesn't exist
         if not arcpy.Exists(out_polygon):
-            poly_paths = out_polygon.split(os.sep)[:-1]
-            poly_path = os.sep.join(poly_paths)
-            poly_name = out_polygon.split(os.sep)[-1]
+            create_zone_fc(temp_polys, sr, out_polygon)
 
-            arcpy.CreateFeatureclass_management(poly_path,
-                                                poly_name,
-                                                template=temp_polys)
-
-        # Create status field if it doesn't exist
-        if cur_status_field not in [f.name for f in arcpy.ListFields(out_polygon)]:
-            arcpy.AddField_management(out_polygon,
-                                      cur_status_field,
-                                      'TEXT',
-                                      field_length=5)
-
-        arcpy.AddField_management(out_polygon,
-                                  cur_date_field,
-                                  'DATE')
+        # Create status fields if they don't exist
+        add_status_fields_to_lyr(out_polygon)
 
         # Set status of all existing features to False
-        where_clause = """{} <> 'False'""".format(cur_status_field)
-        with arcpy.da.UpdateCursor(out_polygon, cur_status_field) as rows:
+        sql = """{} <> 'False'""".format(cur_status_field)
+        with arcpy.da.UpdateCursor(out_polygon,
+                                   cur_status_field,
+                                   where_clause=sql) as rows:
             for row in rows:
                 row[0] = 'False'
                 rows.updateRow(row)
@@ -292,34 +382,13 @@ def main(in_features, date_field, spatial_band_size, temporal_band_size,
         if pub_type in ['ARCGIS_ONLINE', 'ARCGIS_PORTAL'] and pub_polys:
 
             # connect to incidents service
-            fl = connect_to_layer(username, password, server_url, poly_url)
+            try:
+                fl = connect_to_layer(username, password, server_url, poly_url)
+            except:
+                raise Exception('Could not connect to service. Please verify organization URL and service URL are correct, and the provided username and password have access to the service.')
 
             # Check service for status and creation fields - add if necessary
-            layer_fields = [f['name'] for f in fl.fields]
-            fieldToAdd = {"fields": []}
-
-            if not cur_status_field in layer_fields:
-                fieldToAdd["fields"].append({
-                        "name" : cur_status_field,
-                        "type" : "esriFieldTypeString",
-                        "alias" : cur_status_field,
-                        "sqlType" : "sqlTypeOther", "length" : 5,
-                        "nullable" : True,
-                        "editable" : True,
-                        "domain" : None,
-                        "defaultValue" : None})
-
-            if not cur_date_field in layer_fields:
-                fieldToAdd["fields"].append({
-                        "name" : cur_date_field,
-                        "type" : "esriFieldTypeDate",
-                        "alias" : cur_date_field,
-                        "nullable" : True,
-                        "editable" : True,
-                        "domain" : None,
-                        "defaultValue" : None})
-
-            fl.administration.addToDefinition(fieldToAdd)
+            add_status_field_to_service(fl)
 
             # Update 'current' features in service to be 'past'
             field_info = [{'FieldName': cur_status_field,
@@ -341,7 +410,6 @@ def main(in_features, date_field, spatial_band_size, temporal_band_size,
 
             # Add new 'current' features
             fl.addFeatures(temp_polys)
-
 
     except Exception as ex:
         print(ex)
